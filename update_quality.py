a607da2933eef4b64a593953dbdd0f3899a55c56
Shohan Hasan <shohan.hasan@nyu.edu>
2020-04-03 18:34:53 -0400

    (feat) add blue star logic for python

--- update_quality.py
+++ update_quality.py
@@ -1,5 +1,18 @@
+def _update_blue_star(award):
+    """Updates the quality and expires_in of a blue star award."""
+    if award.expires_in > 0:
+        award.quality = max(0, award.quality - 2)
+    else:
+        award.quality = max(0, award.quality - 4)
+    award.expires_in -= 1
+
+
 def update_quality(awards):
     for award in awards:
+        if award.name == 'Blue Star':
+            _update_blue_star(award)
+            continue
+
         if award.name != 'Blue First' and award.name != 'Blue Compare':
             if award.quality > 0:
                 if award.name != 'Blue Distinction Plus':
@@ -29,4 +42,3 @@ def update_quality(awards):
             else:
                 if award.quality < 50:
                     award.quality += 1
-
